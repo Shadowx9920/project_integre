6f8c9a45102408c91bb0a6409890c5721b1c555c
Shadowx9920 <youssefoutahar1@gmail.com>
2022-12-20 22:44:33 +0100

    Added sql connection

--- lib/Android/Views/main_android_page.dart
+++ lib/Android/Views/main_android_page.dart
@@ -10,6 +10,10 @@ class MainAndroidPage extends StatefulWidget {
 class _MainAndroidPageState extends State<MainAndroidPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("Main Android Page"),
+      ),
+    );
   }
 }
